b86229be81813208672f0e307094ecdef161b0c3
Srdjan <srdjan.jokic@proton.me>
2023-02-05 23:58:47 +0100

    cs - scripts - extracting file names from dir.

--- Scripts/generate-toc.py
+++ Scripts/generate-toc.py
@@ -1,9 +1,17 @@
-# "TOC" = Table of Contents
 import os
 
-def extract_toc():
-	files = os.listdir('../')
-	print (files)
+path_to_docs = '../Documents/'
+
+# Iterates through the path_to_docs dir and collects all subdir and file names
+def extract_raw_file_structure():
+	paths = []
+	for path, subdirs, files in os.walk(path_to_docs):
+		for file in files:
+			full_file_path = os.path.join(path, file)
+			file_path = full_file_path.replace(path_to_docs, "")
+			paths.append(file_path)
+
+	print (paths)	
 
 if __name__ == "__main__":
-	extract_toc()
+	extract_raw_file_structure()
