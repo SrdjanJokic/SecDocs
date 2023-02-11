9077c5a5fdeaa90b818ad24b5b82b31caf12436e
Srdjan <srdjan.jokic@proton.me>
2023-02-11 14:52:30 +0100

    cs - fixed tree printing.

--- Scripts/generate-toc.py
+++ Scripts/generate-toc.py
@@ -7,6 +7,9 @@ class Node:
 		self.children = []
 
 	def print(self, indentation_lvl):
+		if (indentation_lvl < 0):
+			return
+
 		for lvl in range(0, indentation_lvl):
 			print('\t', end='')
 
@@ -18,24 +21,28 @@ class Node:
 local_path_to_docs = '../Documents/'
 
 # Prints the given tree in pre-order format
+# Probably can be done a bit cleaner, but not now...
 def print_tree(root):
-	stack = []
-	stack.append(root)
+	nodes_stack = []
+	levels_stack = []
 
-	indent_lvl = 0
-	while (len(stack) != 0):
-		next = stack.pop()
-		while (next != None):
-			next.print(indent_lvl)
+	nodes_stack.append(root)
+	levels_stack.append(-1) # This makes sure root isn't printed
 
-			for i in range (len(next.children) - 1, 0, -1):
-				stack.append(next.children[i])
+	while (len(nodes_stack) != 0):
+		next_node = nodes_stack.pop()
+		indent_lvl = levels_stack.pop()
+		while (next_node != None):
+			next_node.print(indent_lvl)
 
-			if (len(next.children) == 0):
-				next = None
-				indent_lvl -= 1
+			for i in range (len(next_node.children) - 1, 0, -1):
+				nodes_stack.append(next_node.children[i])
+				levels_stack.append(indent_lvl + 1)
+
+			if (len(next_node.children) == 0):
+				next_node = None
 			else:
-				next = next.children[0]
+				next_node = next_node.children[0]
 				indent_lvl += 1
 
 # Creates an m-tree from file system hierarchy
